d7aa9ea0837510e22c1dbe4df7eb42e103b0b9bd
sanchaymittal <sanchaymittal@gmail.com>
2023-03-15 16:04:40 +0700

    new: init target contract

---
+++ contracts/MeanFinanceTarget.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IConnext} from "@connext/interfaces/core/IConnext.sol";
+import {IXReceiver} from "@connext/interfaces/core/IXReceiver.sol";
+
+contract MeanFinanceTarget {
+    // The Connext contract on this domain
+    IConnext public connext;
+
+    modifier onlyConnext() {
+        require(msg.sender == address(connext), "Caller must be Connext");
+        _;
+    }
+
+    constructor(address _connext) {
+        connext = IConnext(_connext);
+    }
+
+    function xReceive(
+        bytes32 _transferId,
+        uint256 _amount, // must be amount in bridge asset less fees
+        address _asset,
+        address _originSender,
+        uint32 _origin,
+        bytes memory _callData
+    ) external onlyConnext returns (bytes memory) {
+        // Decode signed calldata
+        // xCall :ring:
+    }
+}
