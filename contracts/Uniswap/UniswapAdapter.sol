b12f01f48a20659e9dedb8ffdba0c40ce4148405
sanchaymittal <sanchaymittal@gmail.com>
2023-03-16 18:41:57 +0700

    new: adapter for uniswap

---
+++ contracts/Uniswap/UniswapAdapter.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {TransferHelper} from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+
+contract UniswapAdapter {
+    /// @notice UniswapV3 swap router contract to swap
+    /// @dev If deploying to celo, change hardcoded address. see https://docs.uniswap.org/contracts/v3/reference/deployments
+    ISwapRouter public immutable swapRouter =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+
+    function swap(
+        address fromAsset,
+        address toAsset,
+        uint24 poolFee,
+        uint256 amountIn,
+        uint256 amountOutMin
+    ) internal returns (uint256) {
+        // Approve the uniswap router to spend fromAsset.
+        TransferHelper.safeApprove(fromAsset, address(swapRouter), amountIn);
+
+        // Set up uniswap swap params.
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: fromAsset,
+                tokenOut: toAsset,
+                fee: poolFee,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: amountOutMin,
+                sqrtPriceLimitX96: 0
+            });
+
+        // The call to `exactInputSingle` executes the swap.
+        return swapRouter.exactInputSingle(params);
+    }
+}
