64ac8cca69e8b7390ec4e7c103bab2ddf66779e6
sanchaymittal <sanchaymittal@gmail.com>
2023-03-18 01:21:51 +0700

    update config for multiple solidity version support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,22 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.17",
+      },
+    ],
+  },
 };
 
 export default config;
