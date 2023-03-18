f1978d901efc5618e9abfedeaaf1ebe2f83fc26e
sanchaymittal <sanchaymittal@gmail.com>
2023-03-18 20:55:32 +0700

    utils export

--- deploy/index.ts
+++ deploy/index.ts
@@ -1,1 +1,8 @@
-export {DEFAULT_ARGS} from "./00_deploy"
+export const DEFAULT_ARGS: Record<number, string[]> = {
+    31337: [
+      // default to mimic optimism
+      "0x4200000000000000000000000000000000000006", // Weth on Optimism
+      "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", // USDC on Optimism (donation asset)
+      "6648936", // Ethereum domain ID
+    ],
+  };
