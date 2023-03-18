8e9ee27dd1792ed2534abfd8a32d22a30f3431be
sanchaymittal <sanchaymittal@gmail.com>
2023-03-18 23:55:25 +0700

    update config and default args

--- deploy/index.ts
+++ deploy/index.ts
@@ -1,8 +1,9 @@
-export const DEFAULT_ARGS: Record<number, string[]> = {
-    31337: [
-      // default to mimic optimism
-      "0x4200000000000000000000000000000000000006", // Weth on Optimism
-      "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", // USDC on Optimism (donation asset)
-      "6648936", // Ethereum domain ID
-    ],
-  };
+export const DEFAULT_ARGS: Record<number, any> = {
+  // default to mimic optimism
+  31337: {
+    CONNEXT: "0x8f7492DE823025b4CfaAB1D34c58963F2af5DEDA",
+    WETH: "0x4200000000000000000000000000000000000006", // Weth on Optimism
+    USDC: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", // USDC on Optimism
+    DOMAIN: "6648936", // Ethereum domain ID
+  },
+};
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,9 @@ import { HardhatUserConfig } from "hardhat/config";
 envConfig();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+  },
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
@@ -15,51 +17,51 @@ const config: HardhatUserConfig = {
       forking: {
         url: "https://mainnet.optimism.io",
         blockNumber: 73304256, // mined 09/02/2023
-      }
+      },
     },
-  //   // optimism
-  //   optimism: {
-  //     // PRIVATE_KEY loaded from .env file
-  //     accounts: [`0x${process.env.PRIVATE_KEY}`],
-  //     chainId: 10,
-  //     url: "https://mainnet.optimism.io",
-  //   },
-  //   "optimism-goerli": {
-  //     // PRIVATE_KEY loaded from .env file
-  //     accounts: [`0x${process.env.PRIVATE_KEY}`],
-  //     chainId: 420,
-  //     url: "https://endpoints.omniatech.io/v1/op/goerli/public",
-  //   },
-  //   // arbitrum
-  //   "arbitrum-one": {
-  //     accounts: [`0x${process.env.PRIVATE_KEY}`],
-  //     chainId: 42161,
-  //     url: "https://arb1.arbitrum.io/rpc",
-  //   },
-  //   "arbitrum-goerli": {
-  //     accounts: [`0x${process.env.PRIVATE_KEY}`],
-  //     chainId: 421613,
-  //     url: "https://endpoints.omniatech.io/v1/arbitrum/goerli/public",
-  //   },
-  //   // polygon
-  //   polygon: {
-  //     accounts: [`0x${process.env.PRIVATE_KEY}`],
-  //     chainId: 137,
-  //     url: "https://1rpc.io/matic",
-  //   },
-  //   mumbai: {
-  //     accounts: [`0x${process.env.PRIVATE_KEY}`],
-  //     chainId: 80001,
-  //     url: "https://rpc.ankr.com/polygon_mumbai",
-  //   },
-  // },
-  // etherscan: {
-  //   apiKey: {
-  //     // mainnets
-  //     polygon: process.env.POLYGONSCAN_API_KEY!,
-  //     optimisticEthereum: process.env.OPTIMISM_ETHERSCAN_API_KEY!,
-  //     arbitrumOne: process.env.ARBISCAN_API_KEY!,
-  //   }
+    //   // optimism
+    //   optimism: {
+    //     // PRIVATE_KEY loaded from .env file
+    //     accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //     chainId: 10,
+    //     url: "https://mainnet.optimism.io",
+    //   },
+    //   "optimism-goerli": {
+    //     // PRIVATE_KEY loaded from .env file
+    //     accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //     chainId: 420,
+    //     url: "https://endpoints.omniatech.io/v1/op/goerli/public",
+    //   },
+    //   // arbitrum
+    //   "arbitrum-one": {
+    //     accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //     chainId: 42161,
+    //     url: "https://arb1.arbitrum.io/rpc",
+    //   },
+    //   "arbitrum-goerli": {
+    //     accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //     chainId: 421613,
+    //     url: "https://endpoints.omniatech.io/v1/arbitrum/goerli/public",
+    //   },
+    //   // polygon
+    //   polygon: {
+    //     accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //     chainId: 137,
+    //     url: "https://1rpc.io/matic",
+    //   },
+    //   mumbai: {
+    //     accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //     chainId: 80001,
+    //     url: "https://rpc.ankr.com/polygon_mumbai",
+    //   },
+    // },
+    // etherscan: {
+    //   apiKey: {
+    //     // mainnets
+    //     polygon: process.env.POLYGONSCAN_API_KEY!,
+    //     optimisticEthereum: process.env.OPTIMISM_ETHERSCAN_API_KEY!,
+    //     arbitrumOne: process.env.ARBISCAN_API_KEY!,
+    //   }
   },
 };
 
