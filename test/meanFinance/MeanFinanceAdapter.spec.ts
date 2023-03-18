c8d0f6dbe9ca41c48a67e3fe7dab8b3c597fc110
sanchaymittal <sanchaymittal@gmail.com>
2023-03-18 23:56:10 +0700

    updates

--- test/meanFinance/MeanFinanceAdapter.spec.ts
+++ test/meanFinance/MeanFinanceAdapter.spec.ts
@@ -11,8 +11,7 @@ import {
 } from "ethers";
 import { DEFAULT_ARGS } from "../../deploy";
 import { ERC20_ABI } from "@0xgafu/common-abi";
-import {SwapInterval} from "./interval-utils"
-
+import { SwapInterval } from "./interval-utils";
 
 const fund = async (
   asset: string,
@@ -34,7 +33,7 @@ const fund = async (
 
 describe("MeanFinanceAdapter", function () {
   // Set up constants (will mirror what deploy fixture uses)
-  const [WETH, ASSET, DONATION_DOMAIN] = DEFAULT_ARGS[31337];
+  const { WETH, USDC } = DEFAULT_ARGS[31337];
   const MEAN_FINANCE_IDCAHUB = "0xA5AdC5484f9997fBF7D405b9AA62A7d88883C345";
   const WHALE = "0x385BAe68690c1b86e2f1Ad75253d080C14fA6e16"; // this is the address that should have weth, adapter, and random addr
   const NOT_ZERO_ADDRESS = "0x7088C5611dAE159A640d940cde0a3221a4af8896";
@@ -50,7 +49,6 @@ describe("MeanFinanceAdapter", function () {
   let tokenB: Contract;
   let weth: Contract;
   let randomToken: Contract;
-
 
   enum Permission {
     INCREASE,
@@ -74,7 +72,7 @@ describe("MeanFinanceAdapter", function () {
       ethers.provider
     );
     // setup tokens
-    tokenA = new ethers.Contract(ASSET, ERC20_ABI, ethers.provider);
+    tokenA = new ethers.Contract(USDC, ERC20_ABI, ethers.provider);
     weth = new ethers.Contract(WETH, ERC20_ABI, ethers.provider);
     randomToken = new ethers.Contract(RANDOM_TOKEN, ERC20_ABI, ethers.provider);
   });
@@ -85,7 +83,7 @@ describe("MeanFinanceAdapter", function () {
       expect(await adapter.hub()).to.be.eq(MEAN_FINANCE_IDCAHUB);
       // Ensure whale is okay
       expect(whale.address).to.be.eq(WHALE);
-      expect(tokenA.address).to.be.eq(ASSET);
+      expect(tokenA.address).to.be.eq(USDC);
     });
   });
 
@@ -100,7 +98,7 @@ describe("MeanFinanceAdapter", function () {
       );
 
       await fund(
-        ASSET,
+        USDC,
         utils.parseUnits("1", ASSET_DECIMALS),
         whale,
         adapter.address
