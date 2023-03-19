b7d993821d6456d4ec095f7b10cbfafb2e5597ac
sanchaymittal <sanchaymittal@gmail.com>
2023-03-19 13:49:26 +0700

    add: more config for arbitrum and polygon

--- deploy/index.ts
+++ deploy/index.ts
@@ -13,4 +13,18 @@ export const DEFAULT_ARGS: Record<number, any> = {
     USDC: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", // USDC on Optimism
     DOMAIN: "6648936", // Ethereum domain ID
   },
+  // Arbitrum
+  42161: {
+    CONNEXT: "0xEE9deC2712cCE65174B561151701Bf54b99C24C8", // Connext on Arbitrum
+    WETH: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", // Weth on Arbitrum
+    USDC: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", // USDC on Arbitrum (donation asset)
+    DOMAIN: "6648936", // Ethereum domain ID
+  },
+  // Polygon
+  137: {
+    CONNEXT: "0x11984dc4465481512eb5b777E44061C158CF2259", // Connext
+    WETH: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", // Weth
+    USDC: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // USDC
+    DOMAIN: "1886350457", // Ethereum domain ID
+  },
 };
