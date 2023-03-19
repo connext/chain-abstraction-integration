572bdd51b9c8c908ca7afd6e33a5794e222cbf92
sanchaymittal <sanchaymittal@gmail.com>
2023-03-19 13:38:20 +0700

    deployments

--- deploy/index.ts
+++ deploy/index.ts
@@ -6,4 +6,11 @@ export const DEFAULT_ARGS: Record<number, any> = {
     USDC: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", // USDC on Optimism
     DOMAIN: "6648936", // Ethereum domain ID
   },
+  // Optimism
+  10: {
+    CONNEXT: "0x8f7492DE823025b4CfaAB1D34c58963F2af5DEDA",
+    WETH: "0x4200000000000000000000000000000000000006", // Weth on Optimism
+    USDC: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", // USDC on Optimism
+    DOMAIN: "6648936", // Ethereum domain ID
+  },
 };
