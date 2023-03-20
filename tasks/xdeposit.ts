7d73e254af4813552b05f3841bac049a3f0cb1f0
sanchaymittal <sanchaymittal@gmail.com>
2023-03-20 20:47:10 +0700

    update on task

--- tasks/xdeposit.ts
+++ tasks/xdeposit.ts
@@ -1,6 +1,8 @@
 import { Contract } from "ethers";
 import { task } from "hardhat/config";
 
+import { DEFAULT_ARGS } from "../deploy";
+
 type callDataParams = {
   poolFee: number;
   amountOutMin: string;
@@ -22,21 +24,36 @@ type TaskArgs = {
   connextSlippage?: number;
 };
 
-export default task("xdeposit", "Mean finance create position via xcall")
-  .addParam("target", "Mean Finance Target address")
-  .addParam("destination", "destination domain")
-  .addParam("asset", "input asset of ")
-  .setAction(async ({}: TaskArgs, { deployments, ethers }) => {
-    // Get the deployer
-    const [deployer] = await ethers.getSigners();
-    if (!deployer) {
-      throw new Error(`Cannot find signer to deploy with`);
-    }
+export default task(
+  "xdeposit",
+  "Mean finance create position via xcall"
+).setAction(async ({}: TaskArgs, { getChainId, deployments, ethers }) => {
+  // Get the deployer
+  const [deployer] = await ethers.getSigners();
+  if (!deployer) {
+    throw new Error(`Cannot find signer to deploy with`);
+  }
 
-    console.log("deployer: ", deployer.address);
+  console.log("deployer: ", deployer.address);
 
-    const deployment = await deployments.get("MeanFinanceSource");
-    const instance = new Contract(deployment.address, deployment.abi, deployer);
-    console.log("MeanFinanceSource Address: ", deployment.address);
+  const deployment = await deployments.get("MeanFinanceSource");
+  const instance = new Contract(deployment.address, deployment.abi, deployer);
+  console.log("MeanFinanceSource Address: ", deployment.address);
 
-  });
+  // prepare params
+  const { WETH, USDC, CONNEXT, DOMAIN } = DEFAULT_ARGS[10];
+
+  const { WETH: pWETH , USDC: pUSDC, CONNEXT: pCONNEXT, DOMAIN: pDOMAIN } = DEFAULT_ARGS[137];
+
+  // Hardhat coding Target for testing
+
+  const target = "0x3E64213564cc30107Beb81cd0DCEd3F18dF79B35";
+  const destinationDomain = pDOMAIN;
+  
+
+  //     const approveTx = await inputAsset
+  //     .connect(wallet)
+  //     .approve(source.address, inputBalance);
+
+  //   const approveReceipt = await approveTx.wait();
+});
