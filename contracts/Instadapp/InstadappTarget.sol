62ce44f281acaa373d2b28667704f35025c36da6
sanchaymittal <sanchaymittal@gmail.com>
2023-03-22 18:21:41 +0700

    fixes target contract

--- contracts/Instadapp/InstadappTarget.sol
+++ contracts/Instadapp/InstadappTarget.sol
@@ -1,44 +1,50 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IConnext } from "@connext/interfaces/core/IConnext.sol";
-import { IXReceiver } from "@connext/interfaces/core/IXReceiver.sol";
-import { IInstadappTargetAuth, CastData } from "./InstadappTargetAuth.sol";
+import {IConnext} from "@connext/interfaces/core/IConnext.sol";
+import {IXReceiver} from "@connext/interfaces/core/IXReceiver.sol";
+import {InstaTargetAuthInterface as IAT} from "./interfaces/InstaTargetAuthInterface.sol";
 
 contract InstadappTarget is IXReceiver {
-  // Whitelist addresses allowed to  call xReceive
-  // function whitelistAddress()
+    // Whitelist addresses allowed to  call xReceive
+    // function whitelistAddress()
 
-  // The Connext contract on this domain
-  IConnext public connext;
+    // The Connext contract on this domain
+    IConnext public connext;
 
-  // The MetaTxAuthority contract on this domain
-  IInstadappTargetAuth public targetAuth;
+    // The MetaTxAuthority contract on this domain
+    IAT public targetAuth;
 
-  modifier onlyConnext() {
-    require(msg.sender == address(connext), "Caller must be Connext");
-    _;
-  }
+    modifier onlyConnext() {
+        require(msg.sender == address(connext), "Caller must be Connext");
+        _;
+    }
 
-  constructor(address _connext, address _targetAuth) {
-    connext = IConnext(_connext);
-    targetAuth = IInstadappTargetAuth(_targetAuth);
-  }
+    constructor(address _connext, address _targetAuth) {
+        connext = IConnext(_connext);
+        targetAuth = IAT(_targetAuth);
+    }
 
-  function xReceive(
-    bytes32 _transferId,
-    uint256 _amount, // must be amount in bridge asset less fees
-    address _asset,
-    address _originSender,
-    uint32 _origin,
-    bytes memory _callData
-  ) external onlyConnext returns (bytes memory) {
-    // Decode signed calldata
-    (CastData memory _castData, address sender, uint8 v, bytes32 r, bytes32 s) = abi.decode(
-      _callData,
-      (CastData, address, uint8, bytes32, bytes32)
-    );
+    function xReceive(
+        bytes32 _transferId,
+        uint256 _amount, // must be amount in bridge asset less fees
+        address _asset,
+        address _originSender,
+        uint32 _origin,
+        bytes memory _callData
+    ) external onlyConnext returns (bytes memory) {
+        // Decode signed calldata
+        (
+            IAT.CastData memory _castData,
+            address sender,
+            uint8 v,
+            bytes32 r,
+            bytes32 s
+        ) = abi.decode(
+                _callData,
+                (IAT.CastData, address, uint8, bytes32, bytes32)
+            );
 
-    targetAuth.authCast(_castData, sender, v, r, s);
-  }
+        targetAuth.authCast(_castData, sender, v, r, s);
+    }
 }
