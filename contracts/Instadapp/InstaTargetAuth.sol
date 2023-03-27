e59913a90a8136e5cf7a96dadb71ed91a5f36e2b
sanchaymittal <sanchaymittal@gmail.com>
2023-03-27 22:43:11 +0700

    update: Auth function

--- contracts/Instadapp/InstaTargetAuth.sol
+++ contracts/Instadapp/InstaTargetAuth.sol
@@ -8,35 +8,41 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract InstaTargetAuth is EIP712, InstaTargetAuthInterface {
     // Instadapp contract on this domain
-    IDSA public dsa;
-    bytes32 public constant CASTDATA_TYPEHASH = keccak256("CastData(string[] _targetNames,bytes[] _datas,address _origin)");
+    bytes32 public constant CASTDATA_TYPEHASH =
+        keccak256(
+            "CastData(string[] _targetNames,bytes[] _datas,address _origin)"
+        );
 
-    constructor(address _dsa) EIP712("InstaTargetAuth", "1") {
-        dsa = IDSA(_dsa);
-    }
+    constructor() EIP712("InstaTargetAuth", "1") {}
 
     function verify(
+        address auth,
         bytes memory signature,
-        address sender,
         CastData memory castData
     ) public view returns (bool) {
-        bytes32 digest = _hashTypedDataV4(keccak256(
-                abi.encode(CASTDATA_TYPEHASH,
+        bytes32 digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    CASTDATA_TYPEHASH,
                     castData._targetNames,
                     castData._datas,
                     castData._origin
                 )
-            ));
+            )
+        );
         address signer = ECDSA.recover(digest, signature);
-        return signer == sender;
+        return signer == auth;
     }
 
     function authCast(
+        address dsaAddress,
+        address auth,
         bytes memory signature,
-        address sender,
         CastData memory castData
     ) public payable {
-        require(verify(signature, sender, castData), "Invalid signature");
+        IDSA dsa = IDSA(dsaAddress);
+        require(dsa.isAuth(auth), "Invalid Auth");
+        require(verify(auth, signature, castData), "Invalid signature");
 
         // send funds to DSA
         dsa.cast{value: msg.value}(
