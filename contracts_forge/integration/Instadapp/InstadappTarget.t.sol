988e77db52533274fa0a8e43963796da1dc8af8b
wanglonghong <wanglonghong4467@gmail.com>
2023-04-04 22:13:47 +0800

    Create InstadappTarget.t.sol

---
+++ contracts_forge/integration/Instadapp/InstadappTarget.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {TestHelper} from "../../utils/TestHelper.sol";
+
+contract InstadappTargetTest is TestHelper {
+  function setUp() public override {}
+
+  function test_InstadappAdapter__xReceive_shouldRevertIfCallerNotConnext() public {}
+
+  function test_InstadappAdapter__xReceive_shouldRevertIfFallbackAddressInvalid() public {}
+
+  function test_InstadappAdapter__xReceive_shouldWork() public {}
+}
