33f5e82b74610d2c491507e6335f3562ec8e5f79
sanchaymittal <sanchaymittal@gmail.com>
2023-04-06 20:54:00 +0530

    update: adapter for instadapp

--- contracts/integration/Instadapp/InstadappAdapter.sol
+++ contracts/integration/Instadapp/InstadappAdapter.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 import {IDSA} from "./interfaces/IDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract InstadappAdapter is EIP712 {
   struct CastData {
@@ -12,30 +12,52 @@ contract InstadappAdapter is EIP712 {
     address _origin;
   }
 
-  mapping(address => uint256) public nonces;
+  struct Sig {
+    CastData castData;
+    bytes32 salt;
+  }
 
-  // Instadapp contract on this domain
+  mapping(address => bytes32) private sigRelayProtection;
+
   bytes32 public constant CASTDATA_TYPEHASH =
-    keccak256("CastData(string[] _targetNames,bytes[] _datas,address _origin),uint256 nonce");
+    keccak256("CastData(string[] _targetNames,bytes[] _datas,address _origin)");
+
+  bytes32 public constant SIG_TYPEHASH =
+    keccak256("Sig(CastData cast,bytes32 salt)CastData(string[] _targetNames,bytes[] _datas,address _origin)");
 
   constructor() EIP712("InstaTargetAuth", "1") {}
 
   /// Internal functions
-  function authCast(address dsaAddress, address auth, bytes memory signature, CastData memory castData) internal {
+  function authCast(
+    address dsaAddress,
+    address auth,
+    bytes memory signature,
+    CastData memory castData,
+    bytes32 salt
+  ) internal {
     IDSA dsa = IDSA(dsaAddress);
     require(dsa.isAuth(auth), "Invalid Auth");
-    require(verify(auth, signature, castData), "Invalid signature");
-    // increment nonce
-    nonces[auth]++;
+    require(verify(auth, signature, castData, salt), "Invalid signature");
+
+    /// Signature Replay Protection
+    sigRelayProtection[auth] = salt;
+
     // send funds to DSA
     dsa.cast{value: msg.value}(castData._targetNames, castData._datas, castData._origin);
   }
 
-  function verify(address auth, bytes memory signature, CastData memory castData) internal view returns (bool) {
-    bytes32 digest = _hashTypedDataV4(
-      keccak256(abi.encode(CASTDATA_TYPEHASH, castData._targetNames, castData._datas, castData._origin, nonces[auth]))
-    );
+  function verify(
+    address auth,
+    bytes memory signature,
+    CastData memory castData,
+    bytes32 salt
+  ) internal view returns (bool) {
+    bytes32 digest = _hashTypedDataV4(keccak256(abi.encode(SIG_TYPEHASH, hash(castData), salt)));
     address signer = ECDSA.recover(digest, signature);
     return signer == auth;
+  }
+
+  function hash(CastData memory castData) internal pure returns (bytes32) {
+    return keccak256(abi.encode(CASTDATA_TYPEHASH, castData._targetNames, castData._datas, castData._origin));
   }
 }
