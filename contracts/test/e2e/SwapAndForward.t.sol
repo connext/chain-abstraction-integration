7f2515747e6e90a7714cfba1f8a6e9b87c147b10
wanglonghong <wanglonghong4467@gmail.com>
2023-04-07 20:38:33 +0800

    feat: specify solc version

--- contracts/test/e2e/SwapAndForward.t.sol
+++ contracts/test/e2e/SwapAndForward.t.sol
@@ -19,11 +19,15 @@ contract SwapAndForwardTest is TestHelper {
   address immutable WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
   address immutable USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
 
-  function setUp() public override {}
+  function setUp() public override {
+    super.setUp();
+    string memory defaultRpc = "https://arb1.arbitrum.io/rpc";
+    string memory rpc = vm.envOr("ARBITRUM_RPC_URL", defaultRpc);
+    uint256 forkId = vm.createFork(rpc, 77657931);
+    vm.selectFork(forkId);
+  }
 
   function utils_testSetup() public {
-    super.setUp();
-
     greeter = new Greeter();
     xSwapAndGreet = new XSwapAndGreet(address(greeter), MOCK_CONNEXT);
     oneInchUniswapV3 = new OneInchUniswapV3(ARBITRUM_1INCH_SWAPPER);
@@ -43,10 +47,6 @@ contract SwapAndForwardTest is TestHelper {
   }
 
   function test_SwapAndForwardTest__works() public {
-    string memory defaultRpc = "https://arb1.arbitrum.io/rpc";
-    string memory rpc = vm.envOr("ARBITRUM_RPC_URL", defaultRpc);
-    uint256 forkId = vm.createFork(rpc, 77657931);
-    vm.selectFork(forkId);
     utils_testSetup();
 
     vm.prank(MOCK_CONNEXT);
