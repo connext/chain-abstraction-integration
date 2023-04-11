df6e39deff2aabeaa5bca8de3e5193aa8599efa6
just-a-node <eye1717@gmail.com>
2023-04-11 15:25:31 -0600

    Use floating pragma

--- contracts/destination/xreceivers/AuthForwarderXReceiver.sol
+++ contracts/destination/xreceivers/AuthForwarderXReceiver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {IConnext} from "@connext/interfaces/core/IConnext.sol";
