8f0a7e0956575e605e0a5221e80ee3f2a0e7db6d
Rahul Sethuram <rahul@sethuram.com>
2023-04-12 08:45:14 +0400

    Add docs

--- contracts/origin/Swap/SwapAndXCall.sol
+++ contracts/origin/Swap/SwapAndXCall.sol
@@ -8,12 +8,29 @@ import {IConnext} from "connext-interfaces/core/IConnext.sol";
 import {SwapAdapter} from "../../shared/Swap/SwapAdapter.sol";
 
 contract SwapAndXCall is SwapAdapter {
+  // Connext address on this domain
   IConnext connext;
 
   constructor(address _connext) SwapAdapter() {
     connext = IConnext(_connext);
   }
 
+  // EXTERNAL FUNCTIONS
+  /**
+   * @notice Calls a swapper contract and then calls xcall on connext
+   * @dev Data for the swap is generated offchain to call to the appropriate swapper contract
+   * Function is payable since it uses the relayer fee in native asset
+   * @param _fromAsset Address of the asset to swap from
+   * @param _toAsset Address of the asset to swap to
+   * @param _amountIn Amount of the asset to swap from
+   * @param _swapper Address of the swapper contract
+   * @param _swapData Data to call the swapper contract with
+   * @param _destination Destination of the xcall
+   * @param _to Address to send the asset and call with the calldata on the destination
+   * @param _delegate Delegate address
+   * @param _slippage Total slippage amount accepted
+   * @param _callData Calldata to call the destination with
+   */
   function swapAndXCall(
     address _fromAsset,
     address _toAsset,
@@ -39,6 +56,22 @@ contract SwapAndXCall is SwapAdapter {
     );
   }
 
+  /**
+   * @notice Calls a swapper contract and then calls xcall on connext
+   * @dev Data for the swap is generated offchain to call to the appropriate swapper contract
+   * Pays relayer fee from the input asset
+   * @param _fromAsset Address of the asset to swap from
+   * @param _toAsset Address of the asset to swap to
+   * @param _amountIn Amount of the asset to swap from
+   * @param _swapper Address of the swapper contract
+   * @param _swapData Data to call the swapper contract with
+   * @param _destination Destination of the xcall
+   * @param _to Address to send the asset and call with the calldata on the destination
+   * @param _delegate Delegate address
+   * @param _slippage Total slippage amount accepted
+   * @param _callData Calldata to call the destination with
+   * @param _relayerFee Relayer fee to pay in the input asset
+   */
   function swapAndXCall(
     address _fromAsset,
     address _toAsset,
@@ -57,6 +90,18 @@ contract SwapAndXCall is SwapAdapter {
     connext.xcall(_destination, _to, _toAsset, _delegate, amountOut - _relayerFee, _slippage, _callData, _relayerFee);
   }
 
+  // INTERNAL FUNCTIONS
+
+  /**
+   * @notice Sets up the swap and returns the amount out
+   * @dev Handles approvals to the connext contract and the swapper contract
+   * @param _fromAsset Address of the asset to swap from
+   * @param _toAsset Address of the asset to swap to
+   * @param _amountIn Amount of the asset to swap from
+   * @param _swapper Address of the swapper contract
+   * @param _swapData Data to call the swapper contract with
+   * @return amountOut Amount of the asset after swap
+   */
   function _setupAndSwap(
     address _fromAsset,
     address _toAsset,
