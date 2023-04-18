e2dfe37eaad6fda919fbb480d33580e6de755aa7
sanchaymittal <sanchaymittal@gmail.com>
2023-04-18 20:40:17 +0900

    update: Instadapp Target Contract

--- contracts/integration/Instadapp/InstadappTarget.sol
+++ contracts/integration/Instadapp/InstadappTarget.sol
@@ -7,40 +7,45 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {InstadappAdapter} from "./InstadappAdapter.sol";
 
 /// @title InstadappTarget
+/// @author Connext
 /// @notice You can use this contract for cross-chain casting via dsa address
-/// @dev All function calls are currently implemented without side effects
-/// @custom:experimental This needs to have audit.
+/// @dev This contract is used to receive funds from Connext
+/// and forward them to Instadapp DSA via authCast function, In case of failure,
+/// funds are forwarded to fallback address defined by the user under callData.
+/// @custom:experimental This is an experimental contract.
 contract InstadappTarget is IXReceiver, InstadappAdapter {
-  // The Connext contract on this domain
+  /// Storage
+  /// @dev This is the address of the Connext contract.
   IConnext public connext;
 
-  /// EVENTS
+  /// Events
+  /// @dev This event is emitted when the authCast function is called.
   event AuthCast(bytes32 transferId, address dsaAddress, address auth, bool success, bytes returnedData);
 
-  /// MODIFIERS
+  /// Modifiers
+  /// @dev This modifier is used to ensure that only the Connext contract can call the function.
   modifier onlyConnext() {
     require(msg.sender == address(connext), "Caller must be Connext");
     _;
   }
 
+  /// Constructor
+  /// @param _connext The address of the Connext contract.
   constructor(address _connext) {
     connext = IConnext(_connext);
   }
 
-  /**
-   * @notice Receives funds from Connext and forwards them to a fallback address defined by the user under callData,
-   * And forwards call to authCast function
-   * @dev _originSender and _origin are not used in this implementation because this is meant for an "unauthenticated" call. This means
-   * any router can call this function and no guarantees are made on the data passed in. This should only be used when there are
-   * funds passed into the contract that need to be forwarded to another contract. This guarantees economically that there is no
-   * reason to call this function maliciously, because the router would be spending their own funds.
-   * @param _transferId - The transfer ID of the transfer that triggered this call
-   * @param _callData - The data to be prepared and forwarded
-   */
+  /// Public functions
+  /// @dev This function is used to receive funds from Connext and forward them to DSA.
+  /// Then it forwards the call to authCast function.
+  /// @param _amount The amount of funds that will be received.
+  /// @param _asset The address of the asset that will be received.
+  /// @param _transferId The id of the transfer.
+  /// @param _callData The data that will be sent to the targets.
   function xReceive(
     bytes32 _transferId,
-    uint256, // must be amount in bridge asset less fees
-    address,
+    uint256 _amount,
+    address _asset,
     address,
     uint32,
     bytes memory _callData
@@ -51,8 +56,13 @@ contract InstadappTarget is IXReceiver, InstadappAdapter {
       (address, address, bytes, CastData)
     );
 
+    // verify the dsaAddress
     require(dsaAddress != address(0), "!invalidFallback");
 
+    // transfer funds to this dsaAddress
+    IERC20(_asset).transfer(dsaAddress, _amount);
+
+    // forward call to AuthCast
     (bool success, bytes memory returnedData) = address(this).call(
       abi.encodeWithSignature("authCast(address,address,bytes,CastData)", dsaAddress, auth, signature, _castData)
     );
