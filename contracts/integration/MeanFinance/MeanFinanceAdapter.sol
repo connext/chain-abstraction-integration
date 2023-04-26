993d88d4fb7871290d028946c5dffe00587ea242
sanchaymittal <sanchaymittal@gmail.com>
2023-04-26 12:58:59 +0900

    chg: import updates(!minor)

--- contracts/integration/MeanFinance/MeanFinanceAdapter.sol
+++ contracts/integration/MeanFinance/MeanFinanceAdapter.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@mean-finance/dca-v2-core/contracts/interfaces/IDCAHub.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IDCAHub, IDCAPermissionManager} from "@mean-finance/dca-v2-core/contracts/interfaces/IDCAHub.sol";
 
 contract MeanFinanceAdapter {
   /// @notice MeanFinance IDCAHub contract for deposit
