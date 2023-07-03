96a69db3c941067e767e60ae709fdce92bddae05
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2023-07-03 13:10:36 +0530

    feat: added deployment script greeter

---
+++ deploy/Greeter/index.ts
@@ -0,0 +1,36 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { config as envConfig } from "dotenv";
+import { DEFAULT_ARGS } from "../index";
+
+envConfig();
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  // Get the chain id
+  const chainId = +(await hre.getChainId());
+  console.log("chainId", chainId);
+
+  // Get the constructor args
+  const args = [process.env.CONNEXT ?? DEFAULT_ARGS[chainId].CONNEXT];
+
+  // Get the deployer
+  const [deployer] = await hre.ethers.getSigners();
+  if (!deployer) {
+    throw new Error(`Cannot find signer to deploy with`);
+  }
+  console.log("\n============================= Deploying GreeterTarget ===============================");
+  console.log("deployer: ", deployer.address);
+  console.log("constructorArgs:", args);
+
+  // Deploy contract
+  const adapter = await hre.deployments.deploy("GreeterTarget", {
+    from: deployer.address,
+    args: args,
+    skipIfAlreadyDeployed: true,
+    log: true,
+    // deterministicDeployment: true,
+  });
+  console.log(`GreeterTarget deployed to ${adapter.address}`);
+};
+export default func;
+func.tags = ["greetertarget", "test", "prod"];
