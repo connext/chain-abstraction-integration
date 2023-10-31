8f57bfe1bb1f8e4c4f47782683b8e43fed731097
just-a-node <eye1717@gmail.com>
2023-10-30 18:24:33 -0600

    chore: clean out gelato, mean, midas adapters

--- contracts/integration/Midas/interfaces/ICErc20.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import {ICToken} from "./ICToken.sol";
-
-interface ICErc20 is ICToken {
-  function underlying() external view returns (address);
-}
