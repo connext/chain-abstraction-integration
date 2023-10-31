91b08e739e820263eb441312aa595f21326be3b4
just-a-node <eye1717@gmail.com>
2023-10-30 18:26:04 -0600

    feat: adapter uses registry

--- contracts/integration/LockboxAdapter.sol
+++ contracts/integration/LockboxAdapter.sol
@@ -8,25 +8,31 @@ import {IXERC20Lockbox} from "../shared/IXERC20/IXERC20Lockbox.sol";
 import {IConnext} from "@connext/interfaces/core/IConnext.sol";
 import {IXReceiver} from "@connext/interfaces/core/IXReceiver.sol";
 
+interface IXERC20Registry {
+  function ERC20ToXERC20(address erc20) external view returns (address xerc20);
+}
+
 contract LockboxAdapter is IXReceiver {
   address immutable connext;
+  address immutable registry;
 
-  constructor(address _connext) {
+  constructor(address _connext, address _registry) {
     connext = _connext;
+    registry = _registry;
   }
 
   /// @dev Combines Lockbox deposit and xcall using native asset as relayer fee.
   /// @param _destination The destination domain ID.
   /// @param _to The recipient or contract address on destination.
-  /// @param _lockbox The address of the Lockbox, given this contract is only used for xERC20s.
-  /// @param _delegate The address of the Lockbox, given this contract is only used for xERC20s.
+  /// @param _asset The address of the asset to be sent.
+  /// @param _delegate The address on destination allowed to update slippage.
   /// @param _amount The amount of asset to bridge.
   /// @param _slippage The maximum slippage a user is willing to take, in BPS.
   /// @param _callData The data that will be sent to the target contract.
   function xcall(
     uint32 _destination,
     address _to,
-    address _lockbox,
+    address _asset,
     address _delegate,
     uint256 _amount,
     uint256 _slippage,
@@ -34,25 +40,30 @@ contract LockboxAdapter is IXReceiver {
   ) external payable returns (bytes32) {
     require(_amount > 0, "Amount must be greater than 0");
 
-    address xerc20 = IXERC20Lockbox(_lockbox).XERC20();
-    bool isNative = IXERC20Lockbox(_lockbox).IS_NATIVE();
-
-    // Relayer fee is paid in xERC20, so it will also be exchanged in the Lockbox
-    uint256 totalAmount = msg.value;
+    address xerc20 = IXERC20Registry(registry).ERC20ToXERC20(_asset);
+    address lockbox = IXERC20(xerc20).lockbox();
+    address erc20 = IXERC20Lockbox(lockbox).ERC20();
+    bool isNative = IXERC20Lockbox(lockbox).IS_NATIVE();
 
     uint256 _relayerFee;
     if (isNative) {
       require(msg.value >= _amount, "Value sent must be at least equal to the amount specified");
 
+      // Exchange (native) ERC20 for xERC20
+      IXERC20Lockbox(lockbox).depositNative{value: _amount}();
+
       // Assume the rest of msg.value is the relayer fee
       _relayerFee = msg.value - _amount;
-      IXERC20Lockbox(_lockbox).depositNative{value: totalAmount}();
     } else {
+      // Requires user approval
+      IERC20(erc20).transferFrom(msg.sender, address(this), _amount);
+      IERC20(erc20).approve(lockbox, _amount);
+
+      // Exchange ERC20 for xERC20
+      IXERC20Lockbox(lockbox).deposit(_amount);
+
       // The entirety of msg.value is the relayer fee
       _relayerFee = msg.value;
-      IERC20(xerc20).transferFrom(msg.sender, address(this), _amount);
-      IERC20(xerc20).approve(_lockbox, _amount);
-      IXERC20Lockbox(_lockbox).deposit(_amount);
     }
 
     IERC20(xerc20).approve(connext, _amount);
