e34617ab1fb93848288f7e5363c538f044b33c8c
just-a-node <eye1717@gmail.com>
2023-11-01 18:02:43 -0600

    test: fixes to xcall tests and new xreceive test

--- contracts/integration/LockboxAdapter.sol
+++ contracts/integration/LockboxAdapter.sol
@@ -9,7 +9,9 @@ import {IConnext} from "@connext/interfaces/core/IConnext.sol";
 import {IXReceiver} from "@connext/interfaces/core/IXReceiver.sol";
 
 interface IXERC20Registry {
-  function ERC20ToXERC20(address erc20) external view returns (address xerc20);
+  function getXERC20(address erc20) external view returns (address xerc20);
+
+  function getLockbox(address erc20) external view returns (address xerc20);
 }
 
 contract LockboxAdapter is IXReceiver {
@@ -40,30 +42,25 @@ contract LockboxAdapter is IXReceiver {
   ) external payable returns (bytes32) {
     require(_amount > 0, "Amount must be greater than 0");
 
-    address xerc20 = IXERC20Registry(registry).ERC20ToXERC20(_asset);
-    address lockbox = IXERC20(xerc20).lockbox();
-    address erc20 = IXERC20Lockbox(lockbox).ERC20();
+    address xerc20 = IXERC20Registry(registry).getXERC20(_asset);
+    address lockbox = IXERC20Registry(registry).getLockbox(_asset);
     bool isNative = IXERC20Lockbox(lockbox).IS_NATIVE();
 
     uint256 _relayerFee;
     if (isNative) {
       require(msg.value >= _amount, "Value sent must be at least equal to the amount specified");
 
-      // Exchange (native) ERC20 for xERC20
-      IXERC20Lockbox(lockbox).depositNative{value: _amount}();
-
       // Assume the rest of msg.value is the relayer fee
       _relayerFee = msg.value - _amount;
+      IXERC20Lockbox(lockbox).depositNative{value: _amount}();
     } else {
-      // Requires user approval
-      IERC20(erc20).transferFrom(msg.sender, address(this), _amount);
-      IERC20(erc20).approve(lockbox, _amount);
-
-      // Exchange ERC20 for xERC20
-      IXERC20Lockbox(lockbox).deposit(_amount);
+      // IERC20(_asset).transferFrom(msg.sender, address(this), _amount);
+      SafeERC20.safeTransferFrom(IERC20(_asset), msg.sender, address(this), _amount);
+      IERC20(_asset).approve(lockbox, _amount);
 
       // The entirety of msg.value is the relayer fee
       _relayerFee = msg.value;
+      IXERC20Lockbox(lockbox).deposit(_amount);
     }
 
     IERC20(xerc20).approve(connext, _amount);
@@ -71,28 +68,33 @@ contract LockboxAdapter is IXReceiver {
       IConnext(connext).xcall{value: _relayerFee}(_destination, _to, xerc20, _delegate, _amount, _slippage, _callData);
   }
 
-  /// @dev Receives xERC20s from Connext and calls the Lockbox.
-  /// @param _transferId The ID of the transfer.
+  /// @dev Receives xERC20s from Connext and withdraws ERC20 from Lockbox.
   /// @param _amount The amount of funds that will be received.
-  /// @param _asset The address of the asset that will be received.
-  /// @param _callData The data to decode.
+  /// @param _asset The address of the XERC20 that will be received.
+  /// @param _callData The data which should contain the recipient's address.
   function xReceive(
-    bytes32 _transferId,
+    bytes32 /* _transferId */,
     uint256 _amount,
     address _asset,
-    address _originSender,
-    uint32 _origin,
+    address /* _originSender */,
+    uint32 /* _origin */,
     bytes memory _callData
   ) external returns (bytes memory) {
-    // Unpack the _callData to get the recipient's address
-    address _recipient = abi.decode(_callData, (address));
+    address recipient = abi.decode(_callData, (address));
+    address lockbox = IXERC20Registry(registry).getLockbox(_asset);
+    address erc20 = IXERC20Lockbox(lockbox).ERC20();
+    bool isNative = IXERC20Lockbox(lockbox).IS_NATIVE();
 
-    // TODO: how to get lockbox here without registry?
-    // IERC20(_asset).approve(_lockbox, _amount);
+    if (isNative) {
+      // TODO
+    } else {
+      IERC20(_asset).approve(lockbox, _amount);
+      IXERC20Lockbox(lockbox).withdraw(_amount);
+      SafeERC20.safeTransfer(IERC20(erc20), recipient, _amount);
+    }
 
-    // if (_xerc20.allowance(address(this), address(lockbox)) < _amount) {
-    //   _xerc20.approve(address(lockbox), _amount);
-    // }
-    // lockbox.withdraw(_amount);
+    // The xReceive interface requires a bytes memory return value.
+    // This receiver does not need to return any data so it returns an empty bytes array.
+    return "";
   }
 }
--- contracts/test/unit/integration/LockboxAdapter.t.sol
+++ contracts/test/unit/integration/LockboxAdapter.t.sol
@@ -5,9 +5,12 @@ import {TestHelper} from "../../utils/TestHelper.sol";
 import {TransferHelper} from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import {LockboxAdapter} from "../../../../contracts/integration/LockboxAdapter.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IXERC20Lockbox} from "../../../shared/IXERC20/IXERC20Lockbox.sol";
 
 interface IXERC20Registry {
-  function ERC20ToXERC20(address erc20) external view returns (address xerc20);
+  function getXERC20(address erc20) external view returns (address xerc20);
+
+  function getLockbox(address erc20) external view returns (address xerc20);
 }
 
 contract LockboxAdapterTest is TestHelper {
@@ -21,7 +24,16 @@ contract LockboxAdapterTest is TestHelper {
   // TODO: temp mock address
   address registry = address(0x1);
 
-  function utils_setUpOrigin() public {
+  // Default params for xcall
+  uint32 _destination = ARBITRUM_DOMAIN_ID;
+  address _to = USER_CHAIN_A;
+  address _delegate = USER_CHAIN_A;
+  uint256 _slippage = 10000;
+  bytes _callData = "";
+  uint256 _amount = 1e18;
+  uint256 _relayerFee = 1e17;
+
+  function utils_setUpEthereum() public {
     setUpEthereum(18465500); // TODO: update this after registry is deployed
     adapter = new LockboxAdapter(CONNEXT_ETHEREUM, registry);
 
@@ -32,58 +44,112 @@ contract LockboxAdapterTest is TestHelper {
     vm.label(address(this), "AdapterTest");
   }
 
-  function test_LockboxAdapter__xcall_revertsIfZeroAmount(address recipient) public {
-    utils_setUpOrigin();
+  function test_LockboxAdapter__xcall_revertsIfZeroAmount() public {
+    utils_setUpEthereum();
     vm.selectFork(ethereumForkId);
-    assertEq(vm.activeFork(), ethereumForkId);
-
-    uint32 _destination = ARBITRUM_DOMAIN_ID;
-    address _to = USER_CHAIN_A;
-    address _delegate = USER_CHAIN_A;
-    uint256 _slippage = 10000;
-    bytes memory _callData = "";
-    uint256 _amount = 0;
 
     vm.expectRevert(abi.encodePacked("Amount must be greater than 0"));
-
     vm.startPrank(USER_CHAIN_A);
-    adapter.xcall(_destination, _to, lockbox, _delegate, _amount, _slippage, _callData);
+    adapter.xcall(_destination, _to, erc20, _delegate, 0, _slippage, _callData);
     vm.stopPrank();
   }
 
-  function test_LockboxAdapter__xcall_worksWithNonNative(address recipient) public {
-    utils_setUpOrigin();
+  function test_LockboxAdapter__xcall_revertsIfNativeAmountNotEnough() public {
+    utils_setUpEthereum();
     vm.selectFork(ethereumForkId);
-    assertEq(vm.activeFork(), ethereumForkId);
+    vm.deal(USER_CHAIN_A, _amount);
 
+    vm.mockCall(registry, abi.encodeWithSelector(IXERC20Registry.getXERC20.selector, erc20), abi.encode(xerc20));
+    vm.mockCall(registry, abi.encodeWithSelector(IXERC20Registry.getLockbox.selector, erc20), abi.encode(lockbox));
+    vm.mockCall(lockbox, abi.encodeWithSelector(IXERC20Lockbox.IS_NATIVE.selector), abi.encode(true));
+
+    vm.expectRevert(abi.encodePacked("Value sent must be at least equal to the amount specified"));
+    vm.startPrank(USER_CHAIN_A);
+    adapter.xcall{value: _amount - 1}(_destination, _to, erc20, _delegate, _amount, _slippage, _callData);
+    vm.stopPrank();
+  }
+
+  // function test_LockboxAdapter__xcall_worksWithNative(address recipient) public {
+  //   utils_setUpEthereum();
+  //   vm.selectFork(ethereumForkId);
+  //   vm.deal(USER_CHAIN_A, _amount);
+
+  //   uint256 connextInitialAmount = IERC20(xerc20).balanceOf(CONNEXT_ETHEREUM);
+  //   uint256 lockboxInitialAmount = lockbox.balance;
+
+  //   vm.mockCall(registry, abi.encodeWithSelector(IXERC20Registry.getXERC20.selector, erc20), abi.encode(xerc20));
+  //   vm.mockCall(lockbox, abi.encodeWithSelector(IXERC20Lockbox.IS_NATIVE.selector), abi.encode(true));
+
+  //   // TODO: need live native lockbox to test
+  //   vm.mockCall(lockbox, abi.encodeWithSelector(IXERC20Lockbox.depositNative.selector), abi.encode(true));
+
+  //   vm.startPrank(USER_CHAIN_A);
+  //   // (bool success, bytes memory returnData) = address(adapter).call{value: _amount}(
+  //   //   abi.encodeWithSignature(
+  //   //     "xcall(address,address,address,address,uint256,uint256,bytes)",
+  //   //     _destination,
+  //   //     _to,
+  //   //     erc20,
+  //   //     _delegate,
+  //   //     _amount,
+  //   //     _slippage,
+  //   //     _callData
+  //   //   )
+  //   // );
+  //   // if (!success) {
+  //   //   emit log_string("Transaction reverted with message: ");
+  //   //   emit log_bytes(returnData);
+  //   // }
+  //   adapter.xcall{value: _amount}(_destination, _to, erc20, _delegate, _amount, _slippage, _callData);
+  //   vm.stopPrank();
+
+  //   assertEq(USER_CHAIN_A.balance, 0);
+  //   assertEq(lockbox.balance, lockboxInitialAmount + _amount);
+  //   assertEq(IERC20(xerc20).balanceOf(CONNEXT_ETHEREUM), connextInitialAmount + _amount);
+  // }
+
+  function test_LockboxAdapter__xcall_worksWithNonNative() public {
+    utils_setUpEthereum();
+    vm.selectFork(ethereumForkId);
+    deal(erc20, USER_CHAIN_A, _amount);
+    vm.deal(USER_CHAIN_A, _relayerFee);
+
+    uint256 userInitialAmount = IERC20(erc20).balanceOf(USER_CHAIN_A);
+    uint256 lockboxInitialAmount = IERC20(erc20).balanceOf(lockbox);
     uint256 connextInitialAmount = IERC20(xerc20).balanceOf(CONNEXT_ETHEREUM);
 
-    uint32 _destination = ARBITRUM_DOMAIN_ID;
-    address _to = USER_CHAIN_A;
-    address _delegate = USER_CHAIN_A;
-    uint256 _slippage = 10000;
-    bytes memory _callData = "";
-    uint256 _amount = 1e18;
-    uint256 _relayerFee = 1e17;
-
-    deal(erc20, USER_CHAIN_A, _amount);
-    assertEq(IERC20(erc20).balanceOf(USER_CHAIN_A), _amount);
-
-    vm.deal(USER_CHAIN_A, _relayerFee);
-    assertEq(USER_CHAIN_A.balance, _relayerFee);
-
-    vm.mockCall(
-      registry,
-      abi.encodeWithSelector(IXERC20Registry.ERC20ToXERC20.selector, address(erc20)),
-      abi.encode(xerc20)
-    );
+    vm.mockCall(registry, abi.encodeWithSelector(IXERC20Registry.getXERC20.selector, erc20), abi.encode(xerc20));
+    vm.mockCall(registry, abi.encodeWithSelector(IXERC20Registry.getLockbox.selector, erc20), abi.encode(lockbox));
 
     vm.startPrank(USER_CHAIN_A);
     IERC20(erc20).approve(address(adapter), _amount);
-    adapter.xcall{value: _relayerFee}(_destination, _to, lockbox, _delegate, _amount, _slippage, _callData);
+    adapter.xcall{value: _relayerFee}(_destination, _to, erc20, _delegate, _amount, _slippage, _callData);
     vm.stopPrank();
 
-    assertEq(IERC20(erc20).balanceOf(USER_CHAIN_A), 0);
+    assertEq(IERC20(erc20).balanceOf(USER_CHAIN_A), userInitialAmount - _amount);
+    assertEq(IERC20(erc20).balanceOf(lockbox), lockboxInitialAmount + _amount);
     assertEq(IERC20(xerc20).balanceOf(CONNEXT_ETHEREUM), connextInitialAmount + _amount);
+  }
+
+  function test_LockboxAdapter__xReceive_worksWithNonNative() public {
+    utils_setUpEthereum();
+    vm.selectFork(ethereumForkId);
+    deal(xerc20, address(adapter), _amount);
+
+    uint256 userInitialAmount = IERC20(erc20).balanceOf(USER_CHAIN_A);
+    uint256 lockboxInitialAmount = IERC20(erc20).balanceOf(lockbox);
+    uint256 adapterInitialAmount = IERC20(xerc20).balanceOf(address(adapter));
+
+    vm.mockCall(registry, abi.encodeWithSelector(IXERC20Registry.getLockbox.selector, xerc20), abi.encode(lockbox));
+    vm.mockCall(lockbox, abi.encodeWithSelector(IXERC20Lockbox.ERC20.selector), abi.encode(erc20));
+    vm.mockCall(lockbox, abi.encodeWithSelector(IXERC20Lockbox.IS_NATIVE.selector), abi.encode(false));
+
+    vm.startPrank(CONNEXT_ETHEREUM);
+    adapter.xReceive(bytes32(0), _amount, xerc20, USER_CHAIN_A, 1869640809, abi.encode(USER_CHAIN_A));
+    vm.stopPrank();
+
+    assertEq(IERC20(erc20).balanceOf(USER_CHAIN_A), userInitialAmount + _amount);
+    assertEq(IERC20(erc20).balanceOf(lockbox), lockboxInitialAmount - _amount);
+    assertEq(IERC20(xerc20).balanceOf(address(adapter)), adapterInitialAmount - _amount);
   }
 }
