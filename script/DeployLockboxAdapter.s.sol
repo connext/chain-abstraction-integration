b55e390f24c1faea0103098914062075c3b1e07b
just-a-node <eye1717@gmail.com>
2023-12-19 11:00:48 -0700

    chore: clean up unneeded adapters

--- script/DeployLockboxAdapter.s.sol
+++ script/DeployLockboxAdapter.s.sol
@@ -5,13 +5,8 @@ import "forge-std/Script.sol";
 import {LockboxAdapter} from "../contracts/integration/LockboxAdapter.sol";
 
 contract DeployLockboxAdapter is Script {
-  function run() public {
-    // Retrieve the addresses from the command line arguments
-    address connext = vm.envAddress("CONNEXT");
-    address registry = vm.envAddress("REGISTRY");
-    uint256 deployer = vm.envUint("DEPLOYER_PRIVATE_KEY");
-
-    vm.startBroadcast(deployer);
+  function run(address connext, address registry) public {
+    vm.startBroadcast();
 
     new LockboxAdapter(connext, registry);
 
