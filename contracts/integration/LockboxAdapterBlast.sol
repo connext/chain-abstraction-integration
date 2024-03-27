a565a364dbc43a4518eea1a54a1c1351a09b5d48
just-a-node <eye1717@gmail.com>
2024-03-27 12:46:32 -0600

    fix: imports

--- contracts/integration/LockboxAdapterBlast.sol
+++ contracts/integration/LockboxAdapterBlast.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.13;
 
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IXERC20 } from "../../xERC20/interfaces/IXERC20.sol";
-import { IXERC20Lockbox } from "../../xERC20/interfaces/IXERC20Lockbox.sol";
+import { IXERC20 } from "../shared/IXERC20/IXERC20.sol";
+import { IXERC20Lockbox } from "../shared/IXERC20/IXERC20Lockbox.sol";
 
 interface IXERC20Registry {
     function getXERC20(address erc20) external view returns (address xerc20);
